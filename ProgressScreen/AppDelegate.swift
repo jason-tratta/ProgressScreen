a304025fe8c1c62c12251d536114e16187c0d85f
Jason Tratta <jason@jasontratta.com>
2015-10-16 07:16:54 -0500

    Initial Commit

---
+++ ProgressScreen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProgressScreen
+//
+//  Created by Jason Tratta on 10/16/15.
+//  Copyright © 2015 Indiana University. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ProgressScreen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProgressScreen
+//
+//  Created by Jason Tratta on 10/16/15.
+//  Copyright © 2015 Indiana University. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
