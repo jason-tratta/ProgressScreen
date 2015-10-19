90e2ea51f5b7acf2d0243b4f322bd8ec75376d44
Jason Tratta <jtratta@149-160-151-164.dhcp-bl.indiana.edu>
2015-10-19 09:21:19 -0400

    first commit

--- ProgressScreen/ViewController.swift
+++ ProgressScreen/ViewController.swift
@@ -7,13 +7,33 @@
 //
 
 import Cocoa
+import WebKit
 
 class ViewController: NSViewController {
+    
+
+    @IBOutlet var webView: WebView!
+    @IBOutlet var feedbackLabel: NSTextField!
+    @IBOutlet var progressBar: NSProgressIndicator!
+    
+    
+    override func viewDidAppear() {
+        
+        self.view.window?.backgroundColor = NSColor.whiteColor()
+        self.view.window?.collectionBehavior = NSWindowCollectionBehavior.FullScreenPrimary
+        self.view.window?.toggleFullScreen(self)
+        
+        loadWebPage()
+        jssProgress()
+       // logFileTracking()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    
         // Do any additional setup after loading the view.
+
     }
 
     override var representedObject: AnyObject? {
@@ -23,5 +43,77 @@ class ViewController: NSViewController {
     }
 
 
+    
+    func loadWebPage() {
+        
+        let thePath = NSBundle.mainBundle().URLForResource("index", withExtension: "html")
+        webView.mainFrame.loadRequest(NSURLRequest(URL: thePath!))
+        
+        
+    }
+    
+    
+    func jssProgress() {
+        
+        feedbackLabel.stringValue = "Starting Downloads..."
+        self.progressBar.startAnimation(self)
+        
+        
+        
+        
+    }
+    
+    
+    
+    func progress() {
+        
+        
+        let estimatedTime = 60
+        
+        
+        
+        
+    }
+    
+    
+    
+    func  logFileLastRecord() -> String {
+        
+        
+        let log = NSData(contentsOfFile: "/private/var/log/jamf.log")
+        let logString =  NSString(data: log!, encoding: NSUTF8StringEncoding)
+        //debugPrint(logString)
+        
+      
+        let theRange = logString?.rangeOfString("]:", options: NSStringCompareOptions.BackwardsSearch)
+        let scanner = NSScanner(string: logString as! String)
+        scanner.scanLocation = (theRange?.location)!
+        
+        let lineReturn = NSMutableCharacterSet.newlineCharacterSet()
+        
+        var logLine = NSString?()
+        while scanner.scanUpToCharactersFromSet(lineReturn, intoString: &logLine),
+        let logLine = logLine
+        
+        {
+            debugPrint("Log Line")
+            debugPrint(logLine)
+            
+        }
+        
+        
+        let trimTimeStamp = logLine!.stringByReplacingOccurrencesOfString("]:", withString: "")
+        let removedSlash =  trimTimeStamp.stringByReplacingOccurrencesOfString("\"", withString: "")
+        let trimedWhiteSpace = removedSlash.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        
+        debugPrint("Trimmed")
+        debugPrint(trimedWhiteSpace)
+        
+        
+        return trimedWhiteSpace
+    }
+    
+    // End of Class
+    
 }
 
