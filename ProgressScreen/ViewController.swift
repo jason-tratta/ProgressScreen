f0333a538cd937687bbb991279a7b289ad010bf2
Jason Tratta <jtratta@iu.edu>
2015-10-19 11:49:43 -0400

    Progress Timer Implemented

--- ProgressScreen/ViewController.swift
+++ ProgressScreen/ViewController.swift
@@ -16,16 +16,38 @@ class ViewController: NSViewController {
     @IBOutlet var feedbackLabel: NSTextField!
     @IBOutlet var progressBar: NSProgressIndicator!
     
+    var theTimer = NSTimer()
+    var quarter = false
+    var half = false
+    var threeQuarter = false
+    var end = false
+    
+    
+    // Cutomization Variables
+    let estimatedCompletionTime = NSTimeInterval(1800)   //Change 1800 to a time in seconds your installation process averages.
+    
+    // Determine which installer packages should happen in what order in your installation 
+    //This will update the progres bar for a more accurate time estimate.
+    let quarterProgress = "GLOBAL_Cyberduck4.7.2.pkg"
+    let halfProgress = "packageName"
+    let threeQuartersProgress = "packageName"
+    let lastPackage = "packageName"
+    
     
     override func viewDidAppear() {
         
         self.view.window?.backgroundColor = NSColor.whiteColor()
         self.view.window?.collectionBehavior = NSWindowCollectionBehavior.FullScreenPrimary
-        self.view.window?.toggleFullScreen(self)
+        //self.view.window?.toggleFullScreen(self)
+        
+        progressBar.controlTint = NSControlTint.BlueControlTint
+        progressBar.minValue = 0
+        progressBar.maxValue = estimatedCompletionTime
+        progressBar.startAnimation(self)
         
         loadWebPage()
-        jssProgress()
-       // logFileTracking()
+        progress()
+  
     }
 
     override func viewDidLoad() {
@@ -46,39 +68,96 @@ class ViewController: NSViewController {
     
     func loadWebPage() {
         
+        // Uncomment the line below to use a URL instead of embeded HTML
+        // You must then comment out the other var thePath
+        //let thePath = NSURL(string: "http://yourURLhere.com")
+        
         let thePath = NSBundle.mainBundle().URLForResource("index", withExtension: "html")
         webView.mainFrame.loadRequest(NSURLRequest(URL: thePath!))
         
         
     }
     
-    
-    func jssProgress() {
-        
-        feedbackLabel.stringValue = "Starting Downloads..."
-        self.progressBar.startAnimation(self)
-        
-        
-        
-        
-    }
-    
+
     
     
     func progress() {
         
-        
-        let estimatedTime = 60
-        
-        
-        
+        let timerInterval = NSTimeInterval(0.05)
+        theTimer = NSTimer.scheduledTimerWithTimeInterval(timerInterval, target: self, selector: "refreshData", userInfo: nil, repeats: true)
+        theTimer.fire()
         
     }
+    
+    
+    func refreshData() {
+        
+        //refresh the feedback label
+        feedbackLabel.stringValue = logFileLastRecord()
+        
+        
+        //setup the progressbar logic 
+        progressBar.controlTint = NSControlTint.BlueControlTint
+        progressBar.incrementBy(0.05)
+
+        
+        let logString = logFileLastRecord()
+        
+        if (logString.rangeOfString("Successfully installed " + quarterProgress) != nil) {
+            
+            
+            if quarter == false  {
+                
+                quarter = true
+                progressBar.doubleValue = estimatedCompletionTime / 4 }
+            
+    }
+        
+        if (logString.rangeOfString("Successfully installed " + halfProgress) != nil) {
+            
+            
+            if half == false  {
+                
+                half = true
+                progressBar.doubleValue = estimatedCompletionTime / 2 }
+            
+        }
+
+        
+        if (logString.rangeOfString("Successfully installed " + threeQuartersProgress) != nil) {
+            
+            
+            if threeQuarter == false  {
+                
+                threeQuarter = true
+                progressBar.doubleValue = estimatedCompletionTime / 1.33 }
+            
+        }
+
+        
+        
+        if (logString.rangeOfString("Successfully installed " + lastPackage) != nil) {
+            
+            
+            if end == false  {
+                
+                //When the last package is installed, quit this application.
+                end = true
+                NSApplication.sharedApplication().terminate(self)
+            
+            }
+            
+        }
+
+}
+    
+    
     
     
     
     func  logFileLastRecord() -> String {
         
+        //This method reads the jamf.log and returns the last line edited into a easy to read format for the user feedback.
         
         let log = NSData(contentsOfFile: "/private/var/log/jamf.log")
         let logString =  NSString(data: log!, encoding: NSUTF8StringEncoding)
@@ -96,8 +175,8 @@ class ViewController: NSViewController {
         let logLine = logLine
         
         {
-            debugPrint("Log Line")
-            debugPrint(logLine)
+            //debugPrint("Log Line")
+            //debugPrint(logLine)
             
         }
         
@@ -106,8 +185,8 @@ class ViewController: NSViewController {
         let removedSlash =  trimTimeStamp.stringByReplacingOccurrencesOfString("\"", withString: "")
         let trimedWhiteSpace = removedSlash.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
         
-        debugPrint("Trimmed")
-        debugPrint(trimedWhiteSpace)
+        //debugPrint("Trimmed")
+        //debugPrint(trimedWhiteSpace)
         
         
         return trimedWhiteSpace
